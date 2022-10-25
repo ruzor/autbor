a867797687b03bdad70d5304cca03dd0bdbe8b50
ruzor <jwokomaty@gmail.com>
2022-10-25 02:55:04 +0100

    initial coinFlip starter code

---
+++ coinFlipStreaks.py
@@ -0,0 +1,7 @@
+import random
+numberOfStreaks = 0
+for experimentNumber in range(10000):
+    # Code that creates a list of 100 'heads' or 'tails' values.
+
+    # Code that checks if there is a streak of 6 heads or tails in a row.
+print('Chance of streak: %s%%' % (numberOfStreaks / 100))
